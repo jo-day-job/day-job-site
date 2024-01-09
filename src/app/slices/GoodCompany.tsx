f77fb51470ee2dec06431717612eb201ff81e815
samuel <samuelmorganvisual@gmail.com>
2024-01-09 14:45:24 +0000

    fix styling

--- src/app/slices/GoodCompany.tsx
+++ src/app/slices/GoodCompany.tsx
@@ -4,11 +4,6 @@ import { AnimateAllChildren, ScrollOnView } from "easy-chakra-animate";
 import Image from "next/image";
 
 export const GoodCompany = () => {
-  const src = useBreakpointValue({
-    base: "/assets/images/logos-mobile.webp",
-    tablet: "/assets/images/logos.webp",
-    desktop: "/assets/images/logos.webp",
-  });
   return (
     <Flex
       bg="black"
@@ -58,40 +53,62 @@ export const GoodCompany = () => {
         alignItems={{ base: "center", tablet: "flex-start" }}
         textAlign={{ base: "center", tablet: "left" }}
       >
-        <AnimateAllChildren>
-          <Flex
-            direction="column"
-            mt="auto"
-            gap={"24px"}
-            bg="black"
-            rounded={"8px"}
-            px="4px"
-          >
-            <AnimateAllChildren>
-              <Text variant={"h1"} as="h1">
-                You're in good company
-              </Text>
-              <Text variant="body" fontWeight="bold">
-                Over the years, Jo has worked with brands you've heard of (and
-                some you haven't) across all industries and sectors.
-              </Text>
-              <Text variant="body" opacity={0.8}>
-                From fitness influencers to FTSE 500 companies Jo brings a
-                personality punched approach to all projects.
-              </Text>
-            </AnimateAllChildren>
-          </Flex>
-          <Box
-            sx={{
-              img: {
-                w: "auto",
-                h: "fit-content",
-              },
-            }}
-          >
-            <Image alt="" width={395} height={416} src={src ?? ""} />
-          </Box>
-        </AnimateAllChildren>
+        <Flex
+          direction="column"
+          mt="auto"
+          gap={"24px"}
+          bg="black"
+          rounded={"8px"}
+          px="4px"
+        >
+          <AnimateAllChildren>
+            <Text variant={"h1"} as="h1">
+              You're in good company
+            </Text>
+            <Text variant="body" fontWeight="bold">
+              Over the years, Jo has worked with brands you've heard of (and
+              some you haven't) across all industries and sectors.
+            </Text>
+            <Text variant="body" opacity={0.8}>
+              From fitness influencers to FTSE 500 companies Jo brings a
+              personality punched approach to all projects.
+            </Text>
+          </AnimateAllChildren>
+        </Flex>
+        <Box
+          sx={{
+            img: {
+              w: "auto",
+              h: "fit-content",
+            },
+          }}
+          display={{ base: "block", tablet: "none" }}
+        >
+          <Image
+            alt=""
+            width={395}
+            height={416}
+            src={"/assets/images/logos-mobile.webp"}
+          />
+        </Box>
+        <Box
+          sx={{
+            img: {
+              w: "auto",
+              h: "fit-content",
+              minW: "395px",
+              minH: "416px",
+            },
+          }}
+          display={{ base: "none", tablet: "block" }}
+        >
+          <Image
+            alt=""
+            width={395}
+            height={416}
+            src={"/assets/images/logos.webp"}
+          />
+        </Box>
       </Flex>
     </Flex>
   );

e816c17c90b094ed97a1999d76745dbda61d6756
samuel <samuelmorganvisual@gmail.com>
2024-01-09 13:28:08 +0000

    disable ctp

--- src/app/slices/Hero.tsx
+++ src/app/slices/Hero.tsx
@@ -22,75 +22,75 @@ export const Hero = () => {
       <Star />
       <Star />
       <Star />
-      <AnimateAllChildren>
-        <Flex
-          alignItems={{ base: "center", tablet: "flex-end" }}
-          w="100%"
-          h="100%"
-          sx={{
-            img: {
-              transform: "rotate(-5.78deg)",
-              minW: "33vw",
-            },
-          }}
-          position="relative"
-        >
-          <Star />
-          <Star />
-          <Star />
-          <Star />
-          <Star />
+      {/* <AnimateAllChildren> */}
+      <Flex
+        alignItems={{ base: "center", tablet: "flex-end" }}
+        w="100%"
+        h="100%"
+        sx={{
+          img: {
+            transform: "rotate(-5.78deg)",
+            minW: "33vw",
+          },
+        }}
+        position="relative"
+      >
+        <Star />
+        <Star />
+        <Star />
+        <Star />
+        <Star />
 
-          <Image
-            alt=""
-            width={462}
-            height={356}
-            src="/assets/images/Group 51.png"
-          />
-        </Flex>
-        <Flex
-          p={{ base: "16px", tablet: "0" }}
-          justifyContent="flex-end"
-          w="100%"
-          h="100%"
-        >
-          <Image
-            alt=""
-            layout="responsive"
-            width={500}
-            height={500}
-            src={imgSrc}
-          />
-        </Flex>
-        <Flex
-          justifyContent="flex-end"
-          w="100%"
-          h="100%"
-          sx={{
-            img: {
-              transform: "rotate(-5.78deg) scaleX(-1)",
-              mr: "-20px",
-              minW: "33vw",
-            },
-          }}
-          position="relative"
-        >
-          <Star />
-          <Star />
-          <Star />
-          <Star />
-          <Star />
-          <Star />
-          <Star />
+        <Image
+          alt=""
+          width={462}
+          height={356}
+          src="/assets/images/Group 51.png"
+        />
+      </Flex>
+      <Flex
+        p={{ base: "16px", tablet: "0" }}
+        justifyContent="flex-end"
+        w="100%"
+        h="100%"
+      >
+        <Image
+          alt=""
+          layout="responsive"
+          width={500}
+          height={500}
+          src={imgSrc}
+        />
+      </Flex>
+      <Flex
+        justifyContent="flex-end"
+        w="100%"
+        h="100%"
+        sx={{
+          img: {
+            transform: "rotate(-5.78deg) scaleX(-1)",
+            mr: "-20px",
+            minW: "33vw",
+          },
+        }}
+        position="relative"
+      >
+        <Star />
+        <Star />
+        <Star />
+        <Star />
+        <Star />
+        <Star />
+        <Star />
 
-          <Image
-            alt=""
-            width={462.4}
-            height={356.8}
-            src="/assets/images/Group 51.png"
-          />
-        </Flex>
-      </AnimateAllChildren>
+        <Image
+          alt=""
+          width={462.4}
+          height={356.8}
+          src="/assets/images/Group 51.png"
+        />
+      </Flex>
+      {/* </AnimateAllChildren> */}
     </Flex>
   );
 };

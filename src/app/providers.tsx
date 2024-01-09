dc0771ac3766c0ada294922203855a5a66c32c9c
samuel <samuelmorganvisual@gmail.com>
2024-01-09 12:32:28 +0000

    color body

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,7 +2,7 @@
 "use client";
 
 import theme from "@/theme/theme";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, useColorModeValue } from "@chakra-ui/react";
 import { Global } from "@emotion/react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
@@ -15,6 +15,9 @@ export function Providers({ children }: { children: React.ReactNode }) {
           font-weight: 400;
           src: url('/assets/Brockman Font/brockmann-regular-webfont.ttf') format('truetype');
           unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+        }
+        body {
+          background-color: #252525};
         }
       
       `}
